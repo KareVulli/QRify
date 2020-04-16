1efedb93becd720303835348db82b31503ef7aa3
Marvin Helstein <m_helstein@hotmail.com>
2020-04-17 00:53:23 +0300

    First implementation of ScanResultActivity. Updated androidx.appcompat:appcompat to v1.1.0

--- app/src/main/java/eu/niggas_with_attitude/qrify/ScannerActivity.java
+++ app/src/main/java/eu/niggas_with_attitude/qrify/ScannerActivity.java
@@ -3,16 +3,41 @@ package eu.niggas_with_attitude.qrify;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
 import com.journeyapps.barcodescanner.CaptureManager;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+import java.util.List;
 
 public class ScannerActivity extends AppCompatActivity {
 
     private DecoratedBarcodeView barcodeScannerView;
     private CaptureManager capture;
+
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if(result.getText() == null) {
+                return;
+            }
+
+            Intent intent = new Intent(ScannerActivity.this, ScanResultActivity.class);
+            intent.putExtra(ScanResultActivity.EXTRA_RESULT_TEXT, result.getText());
+            // TODO: Save image to storage and pass URI
+            // intent.putExtra(ScanResultActivity.EXTRA_RESULT_IMAGE, result.getBitmap());
+            startActivity(intent);
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +49,7 @@ public class ScannerActivity extends AppCompatActivity {
         capture = new CaptureManager(this, barcodeScannerView);
         capture.initializeFromIntent(getIntent(), savedInstanceState);
         capture.setShowMissingCameraPermissionDialog(false);
-        capture.decode();
+        barcodeScannerView.decodeSingle(callback);
     }
 
     @Override
@@ -46,7 +71,7 @@ public class ScannerActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         capture.onSaveInstanceState(outState);
     }
