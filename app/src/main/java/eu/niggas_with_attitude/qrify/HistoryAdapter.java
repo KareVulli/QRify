a9e11cd354df4da78ca666d5bf9f9e0f634fa2ff
Marvin Helstein <m_helstein@hotmail.com>
2020-04-30 20:35:45 +0300

    Cleaned up code. Decoupled History item onClick listener from adapter.

--- app/src/main/java/eu/niggas_with_attitude/qrify/HistoryAdapter.java
+++ app/src/main/java/eu/niggas_with_attitude/qrify/HistoryAdapter.java
@@ -1,75 +1,58 @@
 package eu.niggas_with_attitude.qrify;
 
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
 import eu.niggas_with_attitude.qrify.database.model.SavedCode;
 
-
-
 public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder> {
 
-    private ClipboardManager clipboardManager;
     private List<SavedCode> data;
-    Context context;
+    private OnHistoryItemClickListener listener;
 
-    public static class HistoryViewHolder extends RecyclerView.ViewHolder {
-
-        public TextView Code;
-
-        public HistoryViewHolder(View v) {
-            super(v);
-            Code = v.findViewById(R.id.history_item_code);
-        }
-    }
-
-
-    public HistoryAdapter(List<SavedCode> data) {
+    HistoryAdapter(List<SavedCode> data, OnHistoryItemClickListener listener) {
         this.data = data;
+        this.listener = listener;
     }
 
+    @NonNull
     @Override
     public HistoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        context = parent.getContext();
-        View v = (View) LayoutInflater.from(parent.getContext())
+        View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fragment_history_item, parent, false);
-
-        HistoryViewHolder vh = new HistoryViewHolder(v);
-        v.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int Position = vh.getAdapterPosition();
-                String selectedCode = data.get(Position).getCode();
-                clipboardManager = (ClipboardManager) v.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clipData = ClipData.newPlainText("text", selectedCode);
-                clipboardManager.setPrimaryClip(clipData);
-                Toast.makeText(context, "Text Copied", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        return vh;
+        return new HistoryViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(HistoryViewHolder holder, int position) {
-        String text = data.get(position).getCode();
-        holder.Code.setText(text);
+        SavedCode code = data.get(position);
+        holder.Code.setText(code.getCode());
+        holder.itemView.setOnClickListener(v -> listener.onItemClick(code));
     }
 
     @Override
     public int getItemCount() {
         return data.size();
+    }
+
+    static class HistoryViewHolder extends RecyclerView.ViewHolder {
+
+        TextView Code;
+
+        HistoryViewHolder(View v) {
+            super(v);
+            Code = v.findViewById(R.id.history_item_code);
+        }
+    }
+
+    public interface OnHistoryItemClickListener {
+        void onItemClick(SavedCode code);
     }
 }
--- app/src/main/java/eu/niggas_with_attitude/qrify/HistoryFragment.java
+++ app/src/main/java/eu/niggas_with_attitude/qrify/HistoryFragment.java
@@ -1,16 +1,21 @@
 package eu.niggas_with_attitude.qrify;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -18,36 +23,38 @@ import eu.niggas_with_attitude.qrify.database.CodeDatabase;
 import eu.niggas_with_attitude.qrify.database.dao.SavedCodeDao;
 import eu.niggas_with_attitude.qrify.database.model.SavedCode;
 
-public class HistoryFragment extends Fragment {
-    public HistoryFragment() {
-        // Required empty public constructor
+public class HistoryFragment extends Fragment implements HistoryAdapter.OnHistoryItemClickListener {
+
+    private ClipboardManager clipboardManager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_history, container, false);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
         CodeDatabase codeDatabase = Room.databaseBuilder(requireContext(),
                 CodeDatabase.class, "code-db").allowMainThreadQueries().build();
         SavedCodeDao savedCodeDao = codeDatabase.getSavedCodeDao();
 
-
-        View rootView = inflater.inflate(R.layout.fragment_history, container, false);
-
-        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.fragment_history_list_view);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        clipboardManager = (ClipboardManager) requireContext().getSystemService(Context.CLIPBOARD_SERVICE);
 
         List<SavedCode> data = savedCodeDao.getAll();
+        HistoryAdapter mAdapter = new HistoryAdapter(data, this);
 
-        HistoryAdapter mAdapter = new HistoryAdapter(data);
-
+        RecyclerView recyclerView = view.findViewById(R.id.fragment_history_list_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setAdapter(mAdapter);
+    }
 
-        return rootView;
+    @Override
+    public void onItemClick(SavedCode code) {
+        ClipData clipData = ClipData.newPlainText("text", code.getCode());
+        clipboardManager.setPrimaryClip(clipData);
+        Toast.makeText(requireContext(), "Text Copied", Toast.LENGTH_SHORT).show();
     }
 }
